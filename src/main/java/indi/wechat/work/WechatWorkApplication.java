3c2411a415ea13c4598567aba1c22165245b0357
mengjun.xiang <mengjun.xiang@gemii.cc>
2020-09-20 22:29:00 +0800

    init： 项目初始化

---
+++ src/main/java/indi/wechat/work/WechatWorkApplication.java
@@ -0,0 +1,13 @@
+package indi.wechat.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatWorkApplication.class, args);
+	}
+
+}
