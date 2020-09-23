ca6bce8fbbbc7a453a2f4e3a9c72ee6b08dee869
mengjun.xiang <mengjun.xiang@gemii.cc>
2020-09-23 22:19:20 +0800

    feat: 增加自建应用回调

--- src/main/java/indi/wechat/work/controller/callback/CallbackController.java
+++ src/main/java/indi/wechat/work/controller/callback/CallbackController.java
@@ -5,6 +5,7 @@ package indi.wechat.work.controller.callback;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,9 +30,21 @@ public class CallbackController {
             @RequestParam(value = "msg_signature") String msgSignature, @RequestParam(value = "timestamp") String timestamp,
             @RequestParam(value = "nonce") String nonce, @RequestParam(value = "echostr", required = false) String echostr) {
         // TODO 获取对应的应用信息
+    	log.info("签名验证");
+        WXBizMsgCrypt extraWxBizMsgCrypt = WXBizMsgCryptConfig.build(agentId, "aesKey", corpId);
+        return null;
+    }
+    @Operation(description = "自建应用回调")
+    @PostMapping(value = "/agent/self/{corpId}/{agentId}")
+    public Object postAgentSelfCallback(@PathVariable("corpId") String corpId, @PathVariable("corpId") String agentId,
+            @RequestParam(value = "msg_signature") String msgSignature, @RequestParam(value = "timestamp") String timestamp,
+            @RequestParam(value = "nonce") String nonce, @RequestParam(value = "echostr", required = false) String echostr) {
+        // TODO 获取对应的应用信息
 
         WXBizMsgCrypt extraWxBizMsgCrypt = WXBizMsgCryptConfig.build(agentId, "aesKey", corpId);
         return null;
     }
 
+    
+    
 }
